8d8414932f5f1c115abd67d9c96a632f3ce86004
Sam Villegas <samvvww@gmail.com>
2022-12-03 11:03:19 -0800

    add styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const routes = require('./routes');
+const path = require('path');
 
 const PORT = process.env.PORT;
 
@@ -8,9 +9,10 @@ const app = express();
 
 app.engine('.html', require('ejs').__express);
 
-console.log('dirname', __dirname);
 app.set('views', path.join(__dirname, 'views'));
+
 app.use('/public', express.static(path.join(__dirname, 'public')));
+
 app.set('view engine', 'html');
 
 const users = [
@@ -23,6 +25,7 @@ const users = [
 app.use(express.json());
 
 //app.use('/auth', authRouter);
+
 routes(app);
 
 app.get('/', (req, res) => {
